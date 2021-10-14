c5f980a065ae725871ebe3a2b7dbc32a9eca9b76
AnastasiiaHryshchenko <92517370+AnastasiiaHryshchenko@users.noreply.github.com>
2021-10-14 17:08:58 +0300

    Connecting to Northwind DB
    Connecting to an existing database using function Reverse Engineering in Entity Framework Core

---
+++ Northwind/Northwind.Core/Views/ProductsAboveAveragePrice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Northwind.Core
+{
+    public partial class ProductsAboveAveragePrice
+    {
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
