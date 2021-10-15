b4ed958809e430e4f4fac5703790d33148f1c946
AnastasiiaHryshchenko <92517370+AnastasiiaHryshchenko@users.noreply.github.com>
2021-10-15 10:02:20 +0300

    The structure of the project has been changed
    Added a library of classes Northwind.Core.Data for working with the database and an empty project Northwind.MVC

---
+++ Northwind/Northwind.MVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Core;
+
+namespace Northwind.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        NorthwindContext db;
+        public HomeController(NorthwindContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }        
+    }
+}
