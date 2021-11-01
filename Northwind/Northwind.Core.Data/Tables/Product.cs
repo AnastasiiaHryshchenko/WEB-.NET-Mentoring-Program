7b7aa5e0fd9a64e96dac94fc04c8cf4c82a15442
AnastasiiaHryshchenko <92517370+AnastasiiaHryshchenko@users.noreply.github.com>
2021-11-01 17:03:23 +0200

    Added validation attributes

--- Northwind/Northwind.Core.Data/Tables/Product.cs
+++ Northwind/Northwind.Core.Data/Tables/Product.cs
@@ -15,12 +15,18 @@ namespace Northwind.Core
 
         [Required(ErrorMessage = "Doesn't contains null or empty values")]
         public int ProductId { get; set; }
+        [Required(ErrorMessage = "Doesn't contains null or empty values")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 200 символов")]
         public string ProductName { get; set; }
         public int? SupplierId { get; set; }
         public int? CategoryId { get; set; }
+        [Required(ErrorMessage = "Doesn't contains null or empty values")]
         public string QuantityPerUnit { get; set; }
+        [Range(0, 999999)]
         public decimal? UnitPrice { get; set; }
+        [Range(0, 9999)]
         public short? UnitsInStock { get; set; }
+        [Range(0, 99)]
         public short? UnitsOnOrder { get; set; }
         public short? ReorderLevel { get; set; }
         [Required(ErrorMessage = "Doesn't contains null or empty values")]
